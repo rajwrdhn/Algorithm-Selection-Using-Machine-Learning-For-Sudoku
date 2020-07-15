64bdd2f6eef149525a5b5eb7abdceff719cf6a59
raj <rajwrdhn>
2020-07-15 20:01:16 +0200

    all instances created until 9x9 via automating

---
+++ Features/benchmark_puzzles/create_instance.py
@@ -0,0 +1,7 @@
+#!usr/var/python3
+from subprocess import call
+
+for i in range(6,10):
+    for j in range(0,105,5):
+        for k in range(1,21):
+            call(["./a.out",  "n%dexample.txt" %i,  "benchmarks%dx%d/%d/puzzle%d.txt" %(i,i,j,k), "%f" %(j/100), "%d" %k])
---
+++ sudoku For MiniZinc/organiseForMinizinc.py
@@ -0,0 +1,12 @@
+#!usr/var/python3
+
+import pandas
+import numpy
+import math
+
+
+def convert_sudoku():
+    return 0
+
+def main():
+    
