cb8c7b94b4e65fd23159b887426398077e8868cd
raj <rajwrdhn>
2020-10-22 09:44:19 +0200

    new experiments

--- read_time/ortool_time.py
+++ read_time/ortool_time.py
@@ -1,2 +1,16 @@
-import sys
+import os.path
 
+def method_iter():
+    lis_or = []
+    for i in range(3,8): 
+        for j in range(0,105,5): 
+            for k in range(1,21):
+                if os.path.exists("/home/raj/Music/SudokuSolvers/Python/data/benchmark_puzzles/benchmarks%dx%d/%d/ortool%d.txt" %(i,i,j,k)):
+                    f = open("/home/raj/Music/SudokuSolvers/Python/data/benchmark_puzzles/benchmarks%dx%d/%d/ortool%d.txt" %(i,i,j,k),"r")
+                    print(f.readlines())
+                else:
+                    print("check me")
+                    continue
+
+                    
+method_iter()
